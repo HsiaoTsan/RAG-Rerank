e0c4d7ae2438b9f3bd34e49da129a5805bf35040
bruce <engineerbrucelee@gmail.com>
2025-01-27 09:58:11 -0500

    Embedding: Support OpenAI and HuggingFace style. OpenAI style can be ChatGPT or DeepSeek.

--- src/ingestion/embedding.py
+++ src/ingestion/embedding.py
@@ -1,8 +1,25 @@
+from openai import OpenAI
+from config.llm_config import llm_config
 from sentence_transformers import SentenceTransformer
 
 class Embedder:
     def __init__(self, model_name="all-MiniLM-L6-v2"):
-        self.model = SentenceTransformer(model_name)
+        if model_name == "ChatGPT":
+            self.model = OpenAI(api_key=llm_config['ChatGPT']['api_key'],
+                                base_url=llm_config['ChatGPT']['base_url'])
+        else:
+            self.model = SentenceTransformer(model_name)
+
+
+
+
 
     def embed(self, texts):
-        return self.model.encode(texts)
+        if isinstance(self.model, OpenAI):
+            return self.model.embeddings.create(
+                            input="texts",
+                            model="text-embedding-3-small"
+                        ).data[0].embedding
+        else:
+            return self.model.encode(texts)
+
